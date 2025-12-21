487c3cb55dba81e7d75715eb13286c2dac61f21e
PolitePie <69392083+CivilPie@users.noreply.github.com>
2025-12-21 16:58:57 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'split-egp-v2';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
